93d758973e8d838b3f86aea4f42401baae1138a6
gaofeng22 <gaofeng22@meituan.com>
2018-04-10 21:30:21 +0800

    test

---
+++ gateway/src/main/java/com/sinosoft/gateway/filter/AccessFilter.java
@@ -0,0 +1,78 @@
+package com.sinosoft.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+* @author gaofeng22
+* @date 2018/4/10
+*/
+@Service
+public class AccessFilter extends ZuulFilter {
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info("send {}  request to {}" , request.getMethod(),request.getRequestURI().toString());
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
